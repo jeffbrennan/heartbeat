2f4f03030a56ffe73a1ce24ba7a1f3373c1bc285
Jeff Brennan <jeffbrennan10@gmail.com>
2025-04-20 20:29:29 -0400

    add 30 second fetch loop

--- heartbeat/backend/main.go
+++ heartbeat/backend/main.go
@@ -4,26 +4,49 @@ import (
 	"context"
 	"fmt"
 	"heartbeat/api"
+	"log"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
-	ctx := context.Background()
-	url := "postgresql://localhost:5432/heartbeat"
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
+	url := "postgresql://localhost:5432/heartbeat"
 	config, err := pgxpool.ParseConfig(url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to parse configuration: %v\n", err)
 		os.Exit(1)
 	}
-	conn, err := pgxpool.NewWithConfig(ctx, config)
+	pool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer conn.Close()
-	api.GetSubwayData(ctx, conn)
+	defer pool.Close()
 
+	var ticks int = 0
+	ticker := time.NewTicker(30 * time.Second)
+
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			ticks++
+			log.Printf("[tick %d] fetching updated subway data", ticks)
+			err = api.GetSubwayData(ctx, pool)
+
+			if err != nil {
+				log.Fatal("Error fetching subway data: ", err)
+			}
+
+		case <-ctx.Done():
+			log.Println("Exiting scheduler")
+			return
+		}
+	}
 }
